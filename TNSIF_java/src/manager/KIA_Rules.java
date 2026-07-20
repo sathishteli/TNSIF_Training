f3daec32be3885a04f0addb9fd92e1fab1bf7115
dell <dell@DESKTOP-3R67SVU>
2026-07-20 10:27:26 +0530

    commit

---
+++ TNSIF_java/src/manager/KIA_Rules.java
@@ -0,0 +1,6 @@
+package manager;
+
+public class KIA_Rules {
+
+
+}
---
+++ TNSIF_java/src/sr_developer/KIA_Internal.java
@@ -0,0 +1,14 @@
+package sr_developer;
+
+import jr_developer.KIA_Airbags;
+
+public class KIA_Internal {
+	
+	public static void main(String[] args) {
+		KIA_Airbags airbag=new KIA_Airbags();
+		airbag.disp_airbags();
+		
+
+	}
+
+}
