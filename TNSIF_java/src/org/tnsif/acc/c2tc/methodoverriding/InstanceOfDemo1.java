beacce40003ecf45434ce64e4c128da061fc4778
sathishteli <sathishmalakappa.cs.nc@cambridge.edu.in>
2026-07-30 10:21:42 +0530

    commit

---
+++ TNSIF_java/src/org/tnsif/acc/c2tc/methodoverriding/InstanceOfDemo1.java
@@ -0,0 +1,34 @@
+package org.tnsif.acc.c2tc.methodoverriding;
+
+class Person5{
+	
+}
+
+class Employee extends Person5{
+	
+}
+
+class Manager extends Employee{
+	
+}
+
+public class InstanceOfDemo1 {
+
+	public static void main(String[] args) {
+		Person5 person = new Person5();
+		Employee emp = new Employee();
+		Manager manager = new Manager();
+		System.out.println(emp instanceof Employee);
+		System.out.println(emp instanceof Person5);
+		System.out.println(emp instanceof Manager);
+		
+		System.out.println(manager instanceof Manager);
+		System.out.println(manager instanceof Person5);
+		System.out.println(manager instanceof Employee);
+		
+		System.out.println(person instanceof Manager);
+		System.out.println(person instanceof Person5);
+		
+	}
+
+}
