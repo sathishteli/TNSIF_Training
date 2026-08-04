9bfbf9325b3bfe39dec0687e4ecdfbb3900499a4
sathishteli <sathishmalakappa.cs.nc@cambridge.edu.in>
2026-08-04 09:55:23 +0530

    commit

---
+++ TNSIF_java/src/org/tnsif/acc/c2tc/scannerclass_bufferreader/ScanerClassDemo1.java
@@ -0,0 +1,21 @@
+package org.tnsif.acc.c2tc.scannerclass_bufferreader;
+
+import java.util.Scanner;
+
+public class ScanerClassDemo1 {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter your name : ");
+		String name = sc.nextLine();
+		
+		System.out.println("enter your age : ");
+		int age = sc.nextInt();
+		
+		System.out.println("my name is : "+ name);
+		System.out.println("my age is : "+ age);
+
+	}
+
+}
