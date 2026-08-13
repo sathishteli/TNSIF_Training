30442d4524010e3eeb8c584342a64708b83e85b4
sathishteli <sathishmalakappa.cs.nc@cambridge.edu.in>
2026-08-13 11:55:50 +0530

    commit

---
+++ TNSIF_java/src/org/tnsif/acc/c2tc/exceptionhandking/ErrorsDemo.java
@@ -0,0 +1,20 @@
+package org.tnsif.acc.c2tc.exceptionhandking;
+
+public class ErrorsDemo {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 20;
+		
+		int sum = add(num1, num2);
+		System.out.println(sum);
+		
+	}
+
+	public static int add(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1+num2;
+	}
+
+}
