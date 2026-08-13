9c8ef145e1bd11cbd89c2ede5c007f7bc52b0c06
sathishteli <sathishmalakappa.cs.nc@cambridge.edu.in>
2026-08-13 12:08:04 +0530

    commit

---
+++ TNSIF_java/src/org/tnsif/acc/c2tc/exceptionhandking/ArithmeticException.java
@@ -0,0 +1,20 @@
+package org.tnsif.acc.c2tc.exceptionhandking;
+
+public class ArithmeticException {
+
+	public static void main(String[] args) {
+		
+		int num1 = 50 / 5;
+		System.out.println(num1);
+		
+		int num2 = 50 / 10;
+		System.out.println(num2);
+		
+		int num3 = 50 / 0;
+		System.out.println(num3);
+		
+		int num4 = 50 / 25;
+		System.out.println(num4);
+	}
+
+}
--- TNSIF_java/src/org/tnsif/acc/c2tc/exceptionhandking/ErrorsDemo.java
+++ TNSIF_java/src/org/tnsif/acc/c2tc/exceptionhandking/ErrorsDemo.java
@@ -5,7 +5,7 @@ public class ErrorsDemo {
 	public static void main(String[] args) {
 		
 		int num1 = 10;
-		int num2 = 20;
+		int num2 = 20; // compile time error when we miss a semicolon
 		
 		int sum = add(num1, num2);
 		System.out.println(sum);
@@ -14,7 +14,7 @@ public class ErrorsDemo {
 
 	public static int add(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return num1+num2;
+		return num1 - num2; //logical error
 	}
 
 }
