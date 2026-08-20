bdeaeabadf1919bf804d6ad048185a01c0cafba8
sathishteli <sathishmalakappa.cs.nc@cambridge.edu.in>
2026-08-20 09:50:54 +0530

    commit

---
+++ TNSIF_java/src/org/tnsif/acc/c2tc/collectiondemo/ComparatorDemo.java
@@ -0,0 +1,69 @@
+package org.tnsif.acc.c2tc.collectiondemo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Person7{
+	
+	String name;
+	int age;
+	
+	public Person7(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	void dispaly() {
+		System.out.println("Name: "+ name +" | Age: "+age);
+	}
+}
+
+class NameComparator implements Comparator<Person7>{
+
+	@Override
+	public int compare(Person7 p1, Person7 p2) 
+	{
+		return p1.name.compareTo(p2.name);
+	}
+	
+}
+
+class AgeComparator implements Comparator<Person6>
+{
+
+	@Override
+	public int compare(Person6 p1, Person6 p2) {
+		
+		return Integer.compare(p1.age,p2.age);
+	}
+	
+}
+
+public class ComparatorDemo {
+
+	public static void main(String[] args) {
+		
+		List<Person7> people = new ArrayList<>();
+		people.add(new Person7("Ram" , 34));
+		people.add(new Person7("Sita" , 24));
+		people.add(new Person7("Ramesh" , 45));
+		
+		Collections.sort(people , new NameComparator());
+		System.out.println("Sorted by name");
+		System.out.println();
+		
+		for(Person7 person : people) {
+			person.dispaly();
+		}
+		
+		System.out.println();
+		
+		Collections.sort(people , new AgeComparator());
+		System.out.println("Sorted by age");
+		System.out.println();
+		
+	}
+
+}
